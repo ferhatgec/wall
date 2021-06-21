f38d9eeb9c6380af5a6686d075a71ed1b7b55ace
ferhatgec <ferhat2gecdogan_fix_me5@protonmail.com>
2021-06-21 23:43:56 +0300

    Add: Source.

---
+++ wall.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+#
+# MIT License
+#
+# Copyright (c) 2021 Ferhat Geçdoğan All Rights Reserved.
+# Distributed under the terms of the MIT License.
+#
+#
+# wall - get & set pseudo-randomly selected wallpapers from unsplash (executable)
+#
+# github.com/ferhatgec/wall
+
+
+from os import environ, system
+from pathlib import Path
+
+from requests import get
+
+unsplash = 'https://source.unsplash.com/random/{res}'
+
+
+class Wall:
+    def __init__(self):
+        self.screen_res = '2048x1152'
+        self.home_path = f'{Path.home()}/wall/'
+
+    def get_image(self):
+        if not Path(self.home_path).exists():
+            Path(self.home_path).mkdir(exist_ok=True)
+
+        with open(f'{self.home_path}wallpaper.jpeg', 'wb') as f:
+            f.write(get(unsplash.format(res=self.screen_res)).content)
+
+        if Path(f'{self.home_path}wallpaper.jpeg').exists():
+            self.set_wallpaper()
+
+    def set_wallpaper(self):
+        desktop_session = environ.get("DESKTOP_SESSION").lower()
+
+        if desktop_session in ['pop', 'gnome', 'cinnamon', 'mate']:
+            system(f'gsettings set org.gnome.desktop.background picture-uri \'file://{self.home_path}wallpaper.jpeg\'')
+        else:
+            print('Uuh! There\'s another desktop env.')
+
+
+init = Wall()
+init.get_image()
